69df9b16e20ac3abfcd8c694afd40a0d6f05b9c8
Oscar Vazquez Mier <ovazquez@elpuig.xeill.net>
2020-10-14 20:25:54 +0200

    Initial and final(?) commit

---
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.navigation;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.navigation.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                // Top-level destinations:
+                R.id.drawer1Fragment, R.id.drawer2Fragment, R.id.bottom1Fragment, R.id.bottom2Fragment, R.id.options1Fragment, R.id.options2Fragment, R.id.tabbed1Fragment
+        )
+                .setOpenableLayout(binding.drawerLayout)
+                .build();
+
+        navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item);
+    }
+}
